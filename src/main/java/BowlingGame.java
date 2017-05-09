68609f7f100e2cec13612bed25c94ee7390213b4
kistyxin <382632082@qq.com>
2017-05-09 17:40:13 +0800

    Update BowlingGame.java

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -7,7 +7,7 @@ public class BowlingGame {
 		 String[] code =new String[11];
 		 int count=0;
 		 int start=0;
-		 //分割字符串用数组存储，存前10局数据
+		
 		 for(int i =0;i<bowlingCode.length();i++){
 			 if(count>9) break;
 			 if(bowlingCode.charAt(i)=='|'){
@@ -16,16 +16,15 @@ public class BowlingGame {
 				 start=i+1;
 			 }
 		 }
-		 //存第11局数据
+		
 		 for(int j=bowlingCode.length()-1;j>=0;j--){
 			 if(bowlingCode.charAt(j)=='|'){
 				 code[10]=bowlingCode.substring(j+1,bowlingCode.length());
 				 break;
 			 }
 		 }
-		 //记录分数的数组，score[i][0]存第一个球分数，score[i][1]存第二个求分数
 		 int[][] score = new int[11][4];
-		 //记录前10局分数
+		
 		 for(int i=0;i<10;i++){
 			 String str = code[i];
 			 int len = str.length();
@@ -46,9 +45,9 @@ public class BowlingGame {
 				 }
 			 }
 		 }
-		 //记录第11局分数
+		
 		 String str=code[10];
-		 //11局的字符串长度
+
 		 int strlen=str.length();
 		 if(strlen==0){
 			 score[10][0]=0;
@@ -72,17 +71,17 @@ public class BowlingGame {
 			 }
 		 }
 		
-		 //score[i][2]为该行总分数
-		 for(int i = 0;i<9;i++){//前9轮
-			 if(score[i][0]==10){//前9轮
-				 if(score[i+1][0]==10){//下局也为strike
+		 
+		 for(int i = 0;i<9;i++){
+			 if(score[i][0]==10){
+				 if(score[i+1][0]==10){
 					 score[i][2]=score[i][0]+score[i+1][0]+score[i+2][0];
 				 }else if(score[i+1][0]!=10||i==8){
 					 score[i][2]=score[i][0]+score[i+1][0]+score[i+1][1];
 				 }
 				 
 			 }else{
-				 //spare,加上下局第一次
+				 
 				 if(score[i][0]+score[i][1]==10){
 					 score[i][2]=score[i][0]+score[i][1]+score[i+1][0];
 				 }else{
@@ -90,7 +89,7 @@ public class BowlingGame {
 				 }
 			 }
 		 }
-		 //第10局分数
+		 
 		 if(score[9][0]==10){
 			 score[9][2]=score[9][0]+score[10][0]+score[10][1];
 		 }
@@ -100,7 +99,7 @@ public class BowlingGame {
 			 score[9][2]=score[9][0]+score[9][1];
 		 }
 		 
-		 //求总分
+	
 		 score[0][3]=score[0][2];
 		 
 		 for(int i=1;i<10;i++){
